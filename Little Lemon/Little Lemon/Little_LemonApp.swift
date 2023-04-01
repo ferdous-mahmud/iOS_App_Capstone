c299a9ec885533de1dd90699214894e06ba3dcf8
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-04-01 16:14:20 +0600

    starter project added

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Ferdous Mahmud Akash on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
