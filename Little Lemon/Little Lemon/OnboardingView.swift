ed15fbb70da5b2e4c34ddeb50c36ff2eba469026
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-04-05 17:34:04 +0600

    onboarding screen component added

--- Little Lemon/Little Lemon/OnboardingView.swift
+++ Little Lemon/Little Lemon/OnboardingView.swift
@@ -8,42 +8,10 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    
-    func resetRegister(){
-        firstName = ""
-        lastName = ""
-        email = ""
-    }
     
     var body: some View {
         VStack{
-            Spacer()
-            TextField("First Name", text: $firstName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 30)
-            TextField("Last Name", text: $lastName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 30)
-            TextField("email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 30)
-            
-            Button("Register") {
-                print(firstName, lastName)
-                print(email)
-                
-                resetRegister()
-            }
-            .buttonStyle(.bordered)
-            .tint(.green)
-            .padding(.top, 10)
-       
-            Spacer()
-            Spacer()
-            Spacer()
+            Text("Hello World")
         }
     }
 }
